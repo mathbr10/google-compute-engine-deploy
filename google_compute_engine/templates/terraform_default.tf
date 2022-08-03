433393ac28427bc9adf2df0d5162c5071db6a605
Matheus Sasso <matheus@pagarba.io>
2022-08-03 11:16:47 -0300

    Revert "feat: create service account through an specified email"
    This reverts commit 4edd684b842069b6d798e07ad5473371c92af687.

--- google_compute_engine/templates/terraform_default.tf
+++ google_compute_engine/templates/terraform_default.tf
@@ -42,11 +42,6 @@ variable "service_account_id" {
   default     = "ai-engine-tf-sa"
 }
 
-variable "default_service_account_email" {
-  description = "Email from default service account"
-  default     = "607243883309-compute@developer.gserviceaccount.com"
-}
-
 
 ################################################################################
 # Resources
@@ -75,67 +70,10 @@ module "gce-container" {
   restart_policy = "Always"
 }
 
-
-##################
-Creating a service account for this deploy - Not working - Can not serve
-##################
-
-#provider "google" {
-#  project     = var.project_id
-#  region      = "us-central1"
-#}
-
-#resource "google_service_account" "created_service_account" {
-#  account_id   = var.service_account_id
-#  display_name = "AI Engine Terraform Service Account"
-#}
-
-#resource "google_service_account_key" "mykey" {
-#  service_account_id = google_service_account.created_service_account.name
-#}
-
-#resource "google_service_account_iam_binding" "admin-account-iam" {
-#  service_account_id = google_service_account.created_service_account.name
-#  role               = "roles/editor"
-
-#  members = [
-#    join(":", ["serviceAccount", google_service_account.created_service_account.email])
-#  ]
-#}
-
-##################
-Creating a service account for this deploy as IAM member - Not working - Can not serve
-##################
-
-#provider "google" {
-#  project     = var.project_id
-#  region      = "us-central1"
-#}
-
-#data "google_compute_default_service_account" "default" {
-#}
-
-#resource "google_service_account" "created_service_account" {
-#  account_id   = var.service_account_id
-#  display_name = "AI Engine Terraform Service Account"
-#}
-
-#resource "google_service_account_key" "mykey" {
-#  service_account_id = google_service_account.created_service_account.name
-#}
-
-#resource "google_service_account_iam_member" "admin-account-iam" {
-#  service_account_id = google_service_account.created_service_account.name
-#  role               = "roles/editor"
-#  member             = "serviceAccount:${google_service_account.created_service_account.email}"
-#}
-
-## Allow SA service account use the default GCE account
-#resource "google_service_account_iam_member" "gce-default-account-iam" {
-#  service_account_id = data.google_compute_default_service_account.default.name
-#  role               = "roles/editor"
-#  member             = "serviceAccount:${google_service_account.created_service_account.email}"
-#}
+resource "google_service_account" "created_service_account" {
+  account_id   = var.service_account_id
+  display_name = "AI Engine Terraform Service Account"
+}
 
 resource "google_compute_instance" "vm" {
   project                   = var.project_id
@@ -165,15 +103,8 @@ resource "google_compute_instance" "vm" {
     container-vm = module.gce-container.vm_container_label
   }
 
-  #service_account {
-  #  email = google_service_account.created_service_account.email
-  #  scopes = [
-  #    "https://www.googleapis.com/auth/cloud-platform",
-  #  ]
-  #}
-
   service_account {
-    email = var.default_service_account_email
+    email = google_service_account.created_service_account.email
     scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
