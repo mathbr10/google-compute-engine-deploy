c411b2d77c630553f22c32f61c3051905da18e05
Matheus Sasso <matheus@pagarba.io>
2022-09-09 11:01:12 -0300

    fix: returining to use de email service account as argument

--- google_compute_engine/templates/terraform_default.tf
+++ google_compute_engine/templates/terraform_default.tf
@@ -61,8 +61,8 @@ variable "gpu_units" {
 # Resources
 ################################################################################
 
-data "google_compute_default_service_account" "default" {
-}
+# data "google_compute_default_service_account" "default" {
+# }
 
 # Data source for container registry image
 data "google_container_registry_image" "bento_service" {
@@ -111,8 +111,15 @@ resource "google_compute_instance" "vm" {
   # metadata_startup_script = "${file("start-up-script.sh")}"
   metadata_startup_script = "${data.template_file.startup_script.rendered}"
 
+  # service_account {
+  #  email = data.google_compute_default_service_account.default.email
+  #  scopes = [
+  #    "https://www.googleapis.com/auth/cloud-platform",
+  #  ]
+  # }
+
   service_account {
-    email = data.google_compute_default_service_account.default.email
+    email = var.default_service_account_email
     scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
